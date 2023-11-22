924636760073417e6167370721313afc23f11f60
Neeraj Kumar <148259510+neerajkumarroy@users.noreply.github.com>
2023-11-22 17:47:29 +0530

    Get_Input_box_value

---
+++ Getinput.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState}from 'react';
+function App(){
+  const[data,setData]=useState(null)
+  const[print,setPrint]=useState(false)
+  function getData(val)
+  {
+    console.warn(val.target.value);
+    setData(val.target.value)
+    setPrint(false)
+
+  }
+
+  return(
+    <div className="App">
+        {
+          print?
+          <h1>{data}</h1>
+          :null
+        }
+        <input type='text' onChange={getData}></input><br/><br/>
+        <button onClick={()=>setPrint(true)}>Get Data</button>    
+       
+            
+      </div>
+
+  )
+}
+
+      
+
+export default App;
