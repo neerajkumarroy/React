ed7ef7d1a0ed46799cc659f17ddef932aa13b53f
Neeraj Kumar <148259510+neerajkumarroy@users.noreply.github.com>
2023-11-24 14:22:12 +0530

    Update Condition.js

--- Condition.js
+++ Condition.js
@@ -14,3 +14,29 @@ function App() {
   )
 }
 export default App;
+
+________________________________________________________________________________________________________________________________________________________
+User.js
+______________________________________________________________________________________________________________________________________________________
+
+import React, { useState } from "react";
+
+function User() {
+  const [loggedIn, setLoggedIn] = useState(2);
+
+  return (
+    <div>
+      {loggedIn === 1 ? 
+        <h1>This is the First Page</h1>
+
+       : loggedIn === 2 ? 
+        <h1>This is the Second Page</h1>
+       
+      : 
+        <h1>This is the Third Page</h1>
+      }
+    </div>
+  );
+}
+
+export default User;
